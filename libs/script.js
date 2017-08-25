fec74e9b302bf64697e4cbd71964e81a56851444
Khairullin Artur <Khairullin Artur>
2017-08-25 14:22:34 +0300

    -fixed

--- libs/script.js
+++ libs/script.js
@@ -29,7 +29,7 @@ $(document).ready(function() {
     // })
     $("img.logo").click(function() {
         window.location.href = "./index.html";
-    })
+    });
 
     if ($('.bxslider').length > 0) {
 
@@ -100,27 +100,55 @@ $(document).ready(function() {
     }
 
     /*SPECIAL OFFERS SLIDER*/
-    if ($('#special-offers ul').length > 0) {
-        var specialOffersSlider = $('#special-offers ul').bxSlider({
+    if ($('.special-offers-block ul').length > 0) {
+        $('.special-offers-block ul').bxSlider({
+            infiniteLoop:false,
+            
             onSlideAfter: function($slideElement, oldIndex, newIndex) {
-                // do mind-blowing JS stuff here
-                $('.special-offers-current-slide-number').html(specialOffersSlider.getCurrentSlide() + 1);
+             
+                $(this).closest('.special-offers-block').find('.special-offers-current-slide-number').html(this.getCurrentSlide()+1);
+            },
+
+            onSliderLoad: function(currentIndex) {
+                //console.log(this);
+                $(this).closest('.special-offers-block').find('.special-offers-overall-slides-number').html(this.getSlideCount());
+                var currentSlider=this;
+                $(this).closest('.special-offers-block').find('.special-offers-slider-next').click(function() {
+                    currentSlider.goToNextSlide();
+                    return false;
+                });
+                $(this).closest('.special-offers-block').find('.special-offers-slider-prev').click(function() {
+                    currentSlider.goToPrevSlide();
+                    return false;
+                });                
+
             }
         });
-
-        var specialOffersSliderCount = specialOffersSlider.getSlideCount();
-        $(".special-offers-overall-slides-number").html(specialOffersSliderCount);
-
-        $('.special-offers-slider-next').click(function() {
-            specialOffersSlider.goToNextSlide();
-            return false;
-        });
-
-        $('.special-offers-slider-prev').click(function() {
-            specialOffersSlider.goToPrevSlide();
-            return false;
-        });
     }
+
+    /*PLANS AND PACKETS SLIDER*/
+
+    $('#plans-and-packets ul').bxSlider({
+            onSlideAfter: function($slideElement, oldIndex, newIndex) {
+             
+                $(this).closest('#plans-and-packets').find('.plans-current-slide-number').html(this.getCurrentSlide()+1);
+            },
+
+            onSliderLoad: function(currentIndex) {
+                //console.log(this);
+                $(this).closest('#plans-and-packets').find('.plans-overall-slides-number').html(this.getSlideCount());
+                var currentSlider=this;
+                $(this).closest('#plans-and-packets').find('#plans-slider-next').click(function() {
+                    currentSlider.goToNextSlide();
+                    return false;
+                });
+                $(this).closest('#plans-and-packets').find('#plans-slider-prev').click(function() {
+                    currentSlider.goToPrevSlide();
+                    return false;
+                });                
+
+            }
+        });
 
     /*SELECTS*/
     $('#district-selector, #town-selector').select2({
@@ -172,4 +200,24 @@ $(document).ready(function() {
 
 
 
+    /*LOAD MORE NEWS*/
+
+    $("#load-more-news").click(function(e) {
+        e.preventDefault();
+        $.ajax({
+            url: 'PATH',
+        })
+        .done(function() {
+            console.log("success");
+        })
+        .fail(function() {
+            console.log("error");
+        })
+        .always(function() {
+            /*БЛОК ЗАГРУЗКИ НОВОСТЕЙ */
+
+            $(".news-container").append('<div class=\"news-item\">\r\n\t\t\t\t\t<div class=\"date\">\r\n\t\t\t\t\t\t<div class=\"day\">26<\/div>\r\n\t\t\t\t\t\t<div class=\"month\">\u043C\u0430\u044F<\/div>\r\n\t\t\t\t\t<\/div>\r\n\r\n\t\t\t\t\t<div class=\"image\">\r\n\t\t\t\t\t\t<img src=\".\/img\/news-image.jpg\">\r\n\t\t\t\t\t<\/div>\r\n\r\n\t\t\t\t\t<div class=\"date-mobile\">\r\n\t\t\t\t\t\t26.05.2017\r\n\t\t\t\t\t<\/div>\r\n\r\n\t\t\t\t\t<div class=\"details\">\r\n\t\t\t\t\t\t<h4>\u0422\u0430\u0442\u0442\u0435\u043B\u0435\u043A\u043E\u043C \u043F\u0440\u0438\u043D\u044F\u043B \u0443\u0447\u0430\u0441\u0442\u0438\u0435 \u0432 \u041A\u0430\u0437\u0430\u043D\u0441\u043A\u043E\u043C \u043C\u0430\u0440\u0430\u0444\u043E\u043D\u0435 \u0438 \u043F\u0440\u0435\u0434\u043E\u0441\u0442\u0430\u0432\u0438\u043B \u0443\u0441\u043B\u0443\u0433\u0438 \u043E\u043D\u043B\u0430\u0439\u043D \u0442\u0440\u0430\u043D\u0441\u043B\u044F\u0446\u0438\u0438<\/h4>\r\n\t\t\t\t\t\t<p>25 \u043C\u0430\u044F \u0432 \u0433. \u0410\u043B\u044C\u043C\u0435\u0442\u044C\u0435\u0432\u0441\u043A\u0435 \u043F\u043E \u0430\u0434\u0440\u0435\u0441\u0443 \u0443\u043B. \u0428\u0435\u0432\u0447\u0435\u043D\u043A\u043E, 47 \u043E\u0442\u043A\u0440\u044B\u043B\u0441\u044F \u043D\u043E\u0432\u044B\u0439 \u043E\u0444\u0438\u0441 \u043F\u0440\u043E\u0434\u0430\u0436 \u0438 \u043E\u0431\u0441\u043B\u0443\u0436\u0438\u0432\u0430\u043D\u0438\u044F \u0422\u0430\u0442\u0442\u0435\u043B\u0435\u043A\u043E\u043C \u0438 \u043C\u043E\u0431\u0438\u043B\u044C\u043D\u043E\u0439 \u0441\u0432\u044F\u0437\u0438 \u041B\u0435\u0442\u0430\u0439.<\/p>\r\n\t\t\t\t\t\t<a href=\"#\">\u0427\u0418\u0422\u0410\u0422\u042C \u0414\u0410\u041B\u0415\u0415<\/a>\r\n\t\t\t\t\t<\/div>\r\n\r\n\t\t\t\t<\/div><div class=\"news-item\">\r\n\t\t\t\t\t<div class=\"date\">\r\n\t\t\t\t\t\t<div class=\"day\">26<\/div>\r\n\t\t\t\t\t\t<div class=\"month\">\u043C\u0430\u044F<\/div>\r\n\t\t\t\t\t<\/div>\r\n\r\n\t\t\t\t\t<div class=\"image\">\r\n\t\t\t\t\t\t<img src=\".\/img\/news-image.jpg\">\r\n\t\t\t\t\t<\/div>\r\n\r\n\t\t\t\t\t<div class=\"date-mobile\">\r\n\t\t\t\t\t\t26.05.2017\r\n\t\t\t\t\t<\/div>\r\n\r\n\t\t\t\t\t<div class=\"details\">\r\n\t\t\t\t\t\t<h4>\u0422\u0430\u0442\u0442\u0435\u043B\u0435\u043A\u043E\u043C \u043F\u0440\u0438\u043D\u044F\u043B \u0443\u0447\u0430\u0441\u0442\u0438\u0435 \u0432 \u041A\u0430\u0437\u0430\u043D\u0441\u043A\u043E\u043C \u043C\u0430\u0440\u0430\u0444\u043E\u043D\u0435 \u0438 \u043F\u0440\u0435\u0434\u043E\u0441\u0442\u0430\u0432\u0438\u043B \u0443\u0441\u043B\u0443\u0433\u0438 \u043E\u043D\u043B\u0430\u0439\u043D \u0442\u0440\u0430\u043D\u0441\u043B\u044F\u0446\u0438\u0438<\/h4>\r\n\t\t\t\t\t\t<p>25 \u043C\u0430\u044F \u0432 \u0433. \u0410\u043B\u044C\u043C\u0435\u0442\u044C\u0435\u0432\u0441\u043A\u0435 \u043F\u043E \u0430\u0434\u0440\u0435\u0441\u0443 \u0443\u043B. \u0428\u0435\u0432\u0447\u0435\u043D\u043A\u043E, 47 \u043E\u0442\u043A\u0440\u044B\u043B\u0441\u044F \u043D\u043E\u0432\u044B\u0439 \u043E\u0444\u0438\u0441 \u043F\u0440\u043E\u0434\u0430\u0436 \u0438 \u043E\u0431\u0441\u043B\u0443\u0436\u0438\u0432\u0430\u043D\u0438\u044F \u0422\u0430\u0442\u0442\u0435\u043B\u0435\u043A\u043E\u043C \u0438 \u043C\u043E\u0431\u0438\u043B\u044C\u043D\u043E\u0439 \u0441\u0432\u044F\u0437\u0438 \u041B\u0435\u0442\u0430\u0439.<\/p>\r\n\t\t\t\t\t\t<a href=\"#\">\u0427\u0418\u0422\u0410\u0422\u042C \u0414\u0410\u041B\u0415\u0415<\/a>\r\n\t\t\t\t\t<\/div>\r\n\r\n\t\t\t\t<\/div><div class=\"news-item\">\r\n\t\t\t\t\t<div class=\"date\">\r\n\t\t\t\t\t\t<div class=\"day\">26<\/div>\r\n\t\t\t\t\t\t<div class=\"month\">\u043C\u0430\u044F<\/div>\r\n\t\t\t\t\t<\/div>\r\n\r\n\t\t\t\t\t<div class=\"image\">\r\n\t\t\t\t\t\t<img src=\".\/img\/news-image.jpg\">\r\n\t\t\t\t\t<\/div>\r\n\r\n\t\t\t\t\t<div class=\"date-mobile\">\r\n\t\t\t\t\t\t26.05.2017\r\n\t\t\t\t\t<\/div>\r\n\r\n\t\t\t\t\t<div class=\"details\">\r\n\t\t\t\t\t\t<h4>\u0422\u0430\u0442\u0442\u0435\u043B\u0435\u043A\u043E\u043C \u043F\u0440\u0438\u043D\u044F\u043B \u0443\u0447\u0430\u0441\u0442\u0438\u0435 \u0432 \u041A\u0430\u0437\u0430\u043D\u0441\u043A\u043E\u043C \u043C\u0430\u0440\u0430\u0444\u043E\u043D\u0435 \u0438 \u043F\u0440\u0435\u0434\u043E\u0441\u0442\u0430\u0432\u0438\u043B \u0443\u0441\u043B\u0443\u0433\u0438 \u043E\u043D\u043B\u0430\u0439\u043D \u0442\u0440\u0430\u043D\u0441\u043B\u044F\u0446\u0438\u0438<\/h4>\r\n\t\t\t\t\t\t<p>25 \u043C\u0430\u044F \u0432 \u0433. \u0410\u043B\u044C\u043C\u0435\u0442\u044C\u0435\u0432\u0441\u043A\u0435 \u043F\u043E \u0430\u0434\u0440\u0435\u0441\u0443 \u0443\u043B. \u0428\u0435\u0432\u0447\u0435\u043D\u043A\u043E, 47 \u043E\u0442\u043A\u0440\u044B\u043B\u0441\u044F \u043D\u043E\u0432\u044B\u0439 \u043E\u0444\u0438\u0441 \u043F\u0440\u043E\u0434\u0430\u0436 \u0438 \u043E\u0431\u0441\u043B\u0443\u0436\u0438\u0432\u0430\u043D\u0438\u044F \u0422\u0430\u0442\u0442\u0435\u043B\u0435\u043A\u043E\u043C \u0438 \u043C\u043E\u0431\u0438\u043B\u044C\u043D\u043E\u0439 \u0441\u0432\u044F\u0437\u0438 \u041B\u0435\u0442\u0430\u0439.<\/p>\r\n\t\t\t\t\t\t<a href=\"#\">\u0427\u0418\u0422\u0410\u0422\u042C \u0414\u0410\u041B\u0415\u0415<\/a>\r\n\t\t\t\t\t<\/div>\r\n\r\n\t\t\t\t<\/div>');
+        });
+        
+    });
 });
